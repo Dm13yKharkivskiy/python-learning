bef7555073aaa81086d58a2fa92e9a350e7a6279
Dm13yKharkovskiy <dmitriy.kharkovskiy@icloud.com>
2023-09-06 10:06:46 +0300

    added task 2 from homework 3

--- hillel/homework/homework3.py
+++ hillel/homework/homework3.py
@@ -1,4 +1,5 @@
 # task 1
+
 try:
     user_select = int(input("Choose a number from 1 to 7: "))
 
@@ -19,6 +20,25 @@ try:
             print("Sunday")
         case _:
             print("Incorrect menu item!")
+except ValueError as error:
+    print("Enter only integer numbers please!")
+    print(f"ValueError: {error}")
+except Exception as error:
+    print(f"Exception: {error}")
+
+# task 2
+
+try:
+    number1, number2 = int(input("Enter first number: ")), int(input("Enter second number: "))
+
+    if number1 == number2:
+        print("The numbers are equal")
+    else:
+        print("Numbers are displayed in ascending order: ")
+        if number1 > number2:
+            print(number2, number1, sep="\n")
+        else:
+            print(number1, number2, sep="\n")
 except ValueError as error:
     print("Enter only integer numbers please!")
     print(f"ValueError: {error}")
